45405fea9986f3077798b08fb6bfcf5647d0f846
James Smith <st23am@gmail.com>
2019-10-02 15:56:07 -0400

    Move those Knaves into a template

---
+++ lib/brave_web/views/character_view.ex
@@ -0,0 +1,18 @@
+defmodule BraveWeb.CharacterView do
+  use BraveWeb, :view
+
+  def display_item(%{name: "None", quality: _quality}), do: ""
+  def display_item(%{name: name, quality: quality}), do: "name: #{name} quality: #{quality}"
+  def display_item(item), do: item
+
+  def items_to_display(inventory) do
+    inventory
+    |> Enum.reject(fn(item) ->
+        if is_map(item) do
+          if item.name == "None" || item.name == "No Armor" do
+            true
+          end
+        end
+    end)
+  end
+end

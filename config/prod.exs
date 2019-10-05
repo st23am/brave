1cb1ce8b181878446a412f6a16f05cb4e9b22810
James Smith <st23am@gmail.com>
2019-10-05 17:29:54 -0400

    rollback force ssl

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :brave, BraveWeb.Endpoint,
   ],
   url: [host: "braveryandknavery.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  force_ssl: [hsts: true],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   pubsub: [name: Brave.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Do not print debug messages in production
